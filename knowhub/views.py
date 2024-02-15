1470aa6911af98b3c5f95d5baf32ef14a33f0db7
Maksim Stahovskij <maksss196@gmail.com>
2024-02-15 23:26:21 +0200

    Added Knowhub app configuration in project settings Included static files configuration for additional directories Added extra URL patterns for home and about pages

--- knowhub/views.py
+++ knowhub/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+posts = [
+    {
+        "author": "MaksST",
+        "title": "Blog 1",
+        "content": "First post content",
+        "date_posted": "February 14, 2024"
+    },
+    {
+        "author": "RaminaST",
+        "title": "Blog 2",
+        "content": "Second post content",
+        "date_posted": "February 15, 2024"
+    }
+]
+
+
+def home(request):
+    context = {
+        "posts": posts
+    }
+    return render(request, "knowhub/home.html", context)
+
+
+def about(request):
+    return render(request, "knowhub/about.html", {"title": "About something"})
