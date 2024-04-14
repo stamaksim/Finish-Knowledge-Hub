73b0e80ad3d08738ecbdf742a862a8f53ee79cf3
Maksim Stahovskij <maksss196@gmail.com>
2024-04-14 18:37:05 +0300

    DEBUG changed from os.environ.get("DJANGO_DEBUG", "") != "False" to "RENDER" not in os.envir...

--- finish_knowledge_hub/settings.py
+++ finish_knowledge_hub/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "Debug")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-
+# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = "RENDER" not in os.environ
 ALLOWED_HOSTS = []
 
 
